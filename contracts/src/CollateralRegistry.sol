c16beb6c8dc67060fd3ba7087f00e753bfdf49d1
lobster <lobster@lobsters-Virtual-Machine.local>
2026-02-05 15:28:12 -0500

    Add CollateralGovernorUpdated event for governance tracking

--- contracts/src/CollateralRegistry.sol
+++ contracts/src/CollateralRegistry.sol
@@ -41,6 +41,7 @@ contract CollateralRegistry is ICollateralRegistry {
     event BaseRateUpdated(uint256 _baseRate);
     event LastFeeOpTimeUpdated(uint256 _lastFeeOpTime);
     event CollateralBranchAdded(uint256 _totalCollaterals, uint256 _index, IERC20Metadata _token, ITroveManager _troveManager, bool _isRedeemable);
+    event CollateralGovernorUpdated(address oldGovernor, address newGovernor);
 
     constructor(IBoldToken _boldToken, IERC20Metadata[] memory _tokens, ITroveManager[] memory _troveManagers, IAeroManager _aeroManager, address _governor) {
         uint256 numTokens = _tokens.length;
@@ -364,7 +365,9 @@ contract CollateralRegistry is ICollateralRegistry {
     }
 
     function updateCollateralGovernor(address _newCollateralGovernor) external onlyGovernor {
+        address oldGovernor = collateralGovernor;
         collateralGovernor = _newCollateralGovernor;
+        emit CollateralGovernorUpdated(oldGovernor, _newCollateralGovernor);
     }
 
     modifier onlyGovernor() {
