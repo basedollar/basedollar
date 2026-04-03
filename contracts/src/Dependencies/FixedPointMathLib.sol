ed8c617fb2911dd3fb1c5de2610c2db7bc3e53c8
geovgy <54918343+geovgy@users.noreply.github.com>
2026-04-03 15:49:30 -0400

    Add contribution

--- contracts/src/Dependencies/FixedPointMathLib.sol
+++ contracts/src/Dependencies/FixedPointMathLib.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.0;
 /// @notice Arithmetic library with operations for fixed-point numbers.
 /// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/utils/FixedPointMathLib.sol)
 /// @author Inspired by USM (https://github.com/usmfum/USM/blob/master/contracts/WadMath.sol)
+/// from dudesahn (https://github.com/dudesahn/PessimisticVelodromeLPOracle/blob/main/contracts/FixedPointMathLib.sol)
 library FixedPointMathLib {
     /*//////////////////////////////////////////////////////////////
                     SIMPLIFIED FIXED POINT OPERATIONS
